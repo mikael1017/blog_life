83c4032599524b911b9499e47d1cc240e7cb3dc3
Jaewoo <jaewoocho1017@gmail.com>
2021-03-30 13:56:08 -0700

    added model

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import BlogPost
+
+
+class SomeModelAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content')
+
+
+admin.site.register(BlogPost, BlogPostAdmin)
 
 # Register your models here.
--- backend/blog/models.py
+++ backend/blog/models.py
@@ -23,4 +23,25 @@ class BlogPost(models.Model):
     slug = models.SlugField()
     category = models.CharField(
         max_length=50, choices=Categories.choices, default=Categories.WORLD)
-# Create your models here.
+    thumbnail = models.ImageField(upload_to='photos/%y/%m/%d/')
+    excerpt = models.CharField(max_length=150)
+    month = models.CharFIeld(max_length=3)
+    day = models.CharField(max_length=2)
+    content = models.TextField()
+    featured = models.BooleanField(default=False)
+    date_created = models.DateTimeField(default=datetime.now, blank=True)
+
+    def save(self, *args, **kwargs):
+        original_slug = slugify(self.title)
+        queryset = BlogPost.objects.all().filter(slug__iexact=original_slug).count()
+
+        count = 1
+        slug = original_slug
+        while(queryset):
+            slug = original_slug + '-' + str(count)
+            count += 1
+            queryset = BlogPost.objects.all().filter(slug__iexact=slug).count()
+
+        self.slug = slug
+
+        #   first-blog-post-1 .... first-blog-post-n
