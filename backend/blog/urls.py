d4b77272896651ceec4b6feaf906f4e13bd68809
Jaewoo <jaewoocho1017@gmail.com>
2021-04-11 16:03:47 -0700

    added url

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', BlogPostListView.as_view()),
     path('featured', BlogPostFeaturedView.as_view()),
     path('category', BlogPostCategoryView.as_view()),
+    path('<slug>', BlogPostDetailView.as_view()),
 ]
