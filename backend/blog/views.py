a6afe57c354ae776a69c238a816a23ba61fa30f2
Jaewoo <jaewoocho1017@gmail.com>
2021-04-12 14:56:31 -0700

    fixed typo

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -3,32 +3,32 @@ from rest_framework import permissions
 from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from blog.models import BlogPost
-from blog.serializers import BlogPostSerializer
+from blog.serializers import UserSerializer
 
 
 class BlogPostListView(ListAPIView):
     queryset = BlogPost.objects.order_by('-date_created')
-    serializer_class = BlogPostSerializer
+    serializer_class = UserSerializer
     lookup_field = 'slug'
     permission_classes = (permissions.AllowAny, )
 
 
 class BlogPostDetailView(RetrieveAPIView):
     queryset = BlogPost.objects.order_by('-date_created')
-    serializer_class = BlogPostSerializer
+    serializer_class = UserSerializer
     lookup_field = 'slug'
     permission_classes = (permissions.AllowAny, )
 
 
 class BlogPostFeaturedView(ListAPIView):
     queryset = BlogPost.objects.all().filter(featured=True)
-    serializer_class = BlogPostSerializer
+    serializer_class = UserSerializer
     lookup_field = 'slug'
     permission_classes = (permissions.AllowAny, )
 
 
 class BlogPostCategoryView(APIView):
-    serializer_class = BlogPostSerializer
+    serializer_class = UserSerializer
     permission_classes = (permissions.AllowAny, )
 
     def post(self, request, format=None):
@@ -37,6 +37,6 @@ class BlogPostCategoryView(APIView):
         queryset = BlogPost.objects.order_by(
             '-date_created').filter(category_iexact=category)
 
-        serializer = BlogPostSerializer(queryset, many=True)
+        serializer = UserSerializer(queryset, many=True)
 
         return Response(serializer.data)
